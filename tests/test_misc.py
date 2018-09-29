227b5bf9449a01682e10a868041fb8ded2110027
Tristan AI-Guru <tristan@ai-guru.de>
2018-09-29 12:38:05 +0200

    Extended test-suite.

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,9 +1,5 @@
 import unittest
-from ngdlm import models as ngdlmodels
-from keras import models, layers
-import logging
-import numpy as np
-import sys
+from ngdlm.models.helpers import append_to_filepath
 import os
 
 
@@ -14,7 +10,7 @@ class TestMisc(unittest.TestCase):
         original_path = os.path.join("folder1", "folder1", "filename.ext")
         target_path = os.path.join("folder1", "folder1", "filename-modified.ext")
 
-        transformed_path = ngdlmodels.append_to_filepath(original_path, "-modified")
+        transformed_path = append_to_filepath(original_path, "-modified")
 
         assert transformed_path == target_path
 
@@ -24,11 +20,9 @@ class TestMisc(unittest.TestCase):
         original_path = os.path.join("folder1", "folder1", "filename")
         target_path = os.path.join("folder1", "folder1", "filename-modified")
 
-        transformed_path = ngdlmodels.append_to_filepath(original_path, "-modified")
+        transformed_path = append_to_filepath(original_path, "-modified")
 
         assert transformed_path == target_path
 
 if __name__ == '__main__':
-    logging.basicConfig( stream=sys.stderr )
-    logging.getLogger( "SomeTest.testSomething" ).setLevel( logging.DEBUG )
     unittest.main()
--- tests/test_models.py
+++ tests/test_models.py
@@ -61,6 +61,7 @@ class TestModels(unittest.TestCase):
 
         # Variational Autoencoder.
         vae = ngdlmodels.VAE(encoder=encoder, decoder=decoder, latent_dim=2)
+        vae.compile(optimizer="rmsprop", loss="mse")
 
         self.assert_load_save_fine(vae)
 
@@ -77,6 +78,7 @@ class TestModels(unittest.TestCase):
 
         # Variational Autoencoder.
         vae = ngdlmodels.VAE(encoder=encoder, decoder=decoder, latent_dim=2)
+        vae.compile(optimizer="rmsprop", loss="mse")
 
         self.assert_load_save_fine(vae)
 
@@ -292,6 +294,53 @@ class TestModels(unittest.TestCase):
 
         prediction = vae.predict_reconstruct_from_latent(np.random.random((1, 2)))
         assert prediction.shape == (1, 100), "Unexpected shape " + str(prediction.shape)
+
+
+    def test_gan(self):
+        # Some parameters.
+        latent_dim = 100
+        input_shape = (28, 28, 1)
+
+        # Generator.
+        generator = models.Sequential()
+        generator.add(layers.Dense(256, input_dim=latent_dim))
+        generator.add(layers.LeakyReLU(alpha=0.2))
+        generator.add(layers.BatchNormalization(momentum=0.8))
+        generator.add(layers.Dense(512))
+        generator.add(layers.LeakyReLU(alpha=0.2))
+        generator.add(layers.BatchNormalization(momentum=0.8))
+        generator.add(layers.Dense(1024))
+        generator.add(layers.LeakyReLU(alpha=0.2))
+        generator.add(layers.BatchNormalization(momentum=0.8))
+        generator.add(layers.Dense(np.prod(input_shape), activation='tanh'))
+        generator.add(layers.Reshape(input_shape))
+
+        # Discriminator.
+        discriminator = models.Sequential()
+        discriminator.add(layers.Flatten(input_shape=input_shape))
+        discriminator.add(layers.Dense(512))
+        discriminator.add(layers.LeakyReLU(alpha=0.2))
+        discriminator.add(layers.Dense(256))
+        discriminator.add(layers.LeakyReLU(alpha=0.2))
+        discriminator.add(layers.Dense(1, activation='sigmoid'))
+        discriminator.compile(
+            loss="binary_crossentropy",
+            optimizer="rmsprop",
+            metrics=["accuracy"])
+
+        # Load and transform the dataset.
+        #(x_input_train, _), (_, _) = mnist.load_data()
+        #x_input_train = x_input_train / 127.5 - 1.
+        #x_input_train = np.expand_dims(x_input_train, axis=3)
+        x_input_train = np.random.random((1, 28, 28, 1))
+
+        # Create the net and train.
+        gan = ngdlmodels.GAN(generator=generator, discriminator=discriminator)
+        gan.compile(
+            optimizer="rmsprop",
+            loss="binary_crossentropy",
+            metrics=['accuracy'])
+        history = gan.fit(x_input_train, epochs=1, batch_size=32, sample_interval=None)
 
 
     def tearDown(self):
